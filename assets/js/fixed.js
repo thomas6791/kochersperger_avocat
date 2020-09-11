bb027446eddcee5156f589343acfd4c9357ab904
thomas hunckler <hunckler.thomas@hotmail.fr>
2020-09-11 23:03:23 +0200

    change

--- assets/js/fixed.js
+++ assets/js/fixed.js
@@ -3,8 +3,8 @@ const logo = elementoFixed.querySelector(".logo");
 window.addEventListener("scroll", (event) => {
   if (window.pageYOffset >= 5) {
     elementoFixed.classList.add("fixed");
-    elementoFixed.style.fontSize = "0.8em";
-    logo.style.width = "40px";
+    elementoFixed.style.fontSize = "0.9em";
+    logo.style.width = "45px";
   } else {
     elementoFixed.classList.remove("fixed");
     elementoFixed.style.fontSize = "1em";
